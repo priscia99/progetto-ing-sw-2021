67543af594908c032d5979325535b3f0c259dad6
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-04-05 00:00:37 +0200

    refactored

--- Code/src/main/java/it/polimi/ingsw/utils/CustomLogger.java
+++ Code/src/main/java/it/polimi/ingsw/utils/CustomLogger.java
@@ -1,30 +1,35 @@
 package it.polimi.ingsw.utils;
 import java.util.Date;
+import java.util.Objects;
 import java.util.logging.*;
 
 public class CustomLogger {
     static private Logger customLogger;
 
-    static public Logger getLogger(){
-        if(customLogger==null){
-            customLogger = Logger.getLogger(CustomLogger.class.getName());
-            customLogger.setUseParentHandlers(false);
-            ConsoleHandler handler = new ConsoleHandler();
-            handler.setFormatter(new SimpleFormatter() {
-                private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+    static private void initLogger(){
+        customLogger = Logger.getLogger(CustomLogger.class.getName());
+        customLogger.setUseParentHandlers(false);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
 
-                @Override
-                public synchronized String format(LogRecord logRecord) {
-                    return String.format(format,
-                            new Date(logRecord.getMillis()),
-                            logRecord.getLevel().getLocalizedName(),
-                            logRecord.getMessage()
-                    );
-                }
-            });
-            customLogger.addHandler(handler);
-            customLogger.setLevel(Level.ALL);
-        }
+            //Sample output: [2021-04-04 23:48:24] [INFO   ] test
+            private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+
+            @Override
+            public synchronized String format(LogRecord logRecord) {
+                return String.format(format,
+                        new Date(logRecord.getMillis()),
+                        logRecord.getLevel().getLocalizedName(),
+                        logRecord.getMessage()
+                );
+            }
+        });
+        customLogger.addHandler(handler);
+        customLogger.setLevel(Level.ALL);
+    }
+
+    static public Logger getLogger(){
+        if(Objects.isNull(customLogger)) initLogger();
         return customLogger;
     }
 }
