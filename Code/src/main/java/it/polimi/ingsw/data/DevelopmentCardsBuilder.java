9e944850169bf0769b25736ede77b705ed79ad8b
Andrea Prisciantelli <priscia99@gmail.com>
2021-04-06 00:37:13 +0200

    Add 'data' package with builders - Add DevelopmentCardsBuilder - Add LeaderCardsBuilder - Coded getDeck() function of all these classes

---
+++ Code/src/main/java/it/polimi/ingsw/data/DevelopmentCardsBuilder.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.data;
+
+import it.polimi.ingsw.model.card.DevelopmentCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Objects;
+
+public class DevelopmentCardsBuilder {
+    List<DevelopmentCard> developmentCardList;
+
+    private void initBuilder(){
+        // TODO: Code DevelopmentCardsBuilder init function
+    }
+
+    public List<DevelopmentCard> getDeck(){
+        if(Objects.isNull(developmentCardList)) initBuilder();
+        return new ArrayList<>(developmentCardList);
+    }
+}
---
+++ Code/src/main/java/it/polimi/ingsw/data/LeaderCardsBuilder.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.data;
+
+import it.polimi.ingsw.model.card.LeaderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class LeaderCardsBuilder{
+    List<LeaderCard> leaderCardList;
+
+    private void initBuilder(){
+        // TODO: Code LeaderCardsBuilder init function
+    }
+
+    public List<LeaderCard> getDeck(){
+        if(Objects.isNull(leaderCardList)) initBuilder();
+        return new ArrayList<>(leaderCardList);
+    }
+}
