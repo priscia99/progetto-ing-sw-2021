f885c8761f5d8abaad66cd0d39581dfc2edeb5a1
Andrea Prisciantelli <priscia99@gmail.com>
2021-04-07 23:41:17 +0200

    Added removeLeaderCard() and addLeader() function in LeaderCardsDeck Fixed pop() function in LeaderCardsDeck Added EmptyDeckException

--- Code/src/main/java/it/polimi/ingsw/model/player_board/LeaderCardsDeck.java
+++ Code/src/main/java/it/polimi/ingsw/model/player_board/LeaderCardsDeck.java
@@ -1,22 +1,19 @@
 package it.polimi.ingsw.model.player_board;
 
 import it.polimi.ingsw.data.LeaderCardsBuilder;
+import it.polimi.ingsw.exceptions.EmptyDeckException;
 import it.polimi.ingsw.model.card.LeaderCard;
 import it.polimi.ingsw.model.card.effect.DiscountEffect;
 import it.polimi.ingsw.model.card.effect.EffectType;
 import it.polimi.ingsw.model.resource.ResourceType;
 import it.polimi.ingsw.utils.CustomLogger;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class LeaderCardsDeck {
 
     private final ArrayList<LeaderCard> leaderCards;
 
-    // TODO: Remove this function and call builder in setupLeaderCards()
     static public LeaderCardsDeck getStartingDeck(){
         return new LeaderCardsDeck((ArrayList<LeaderCard>)LeaderCardsBuilder.getDeck());
     }
@@ -34,7 +31,7 @@ public class LeaderCardsDeck {
     }
 
     public LeaderCard pop(){
-        //TODO: add error if deck is empty
+        if(leaderCards.isEmpty()) throw new EmptyDeckException("This player's leader cards deck is empty.");
         return this.leaderCards.remove(0);
     }
 
@@ -44,10 +41,12 @@ public class LeaderCardsDeck {
     }
 
     public void addLeader(LeaderCard leaderCard) {
-        // TODO: implement addLeader();
+        leaderCards.add(leaderCard);
     }
 
     public void removeLeaderCard(LeaderCard leaderCard) {
-        // TODO: implement removeLeaderCard();
+        if(leaderCards.isEmpty()) throw new EmptyDeckException("This player's leader cards deck is empty.");
+        if(leaderCards.remove(leaderCard) == false)
+            throw new IllegalArgumentException("LeaderCard not present this player's deck");
     }
 }
