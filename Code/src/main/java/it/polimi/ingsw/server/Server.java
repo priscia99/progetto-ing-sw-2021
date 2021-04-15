27b09ebbed97dd1b68842e807f6b58469fbf967e
P1224 <giacomo.pizza99@gmail.com>
2021-04-15 23:31:01 +0200

    Implemented client/server classes

--- Code/src/main/java/it/polimi/ingsw/server/Server.java
+++ Code/src/main/java/it/polimi/ingsw/server/Server.java
@@ -1,23 +1,4 @@
 package it.polimi.ingsw.server;
 
-import it.polimi.ingsw.model.game.Game;
-
-import java.net.ServerSocket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Server {
-
-    private static final int PORT = 8888;
-    private ServerSocket serverSocket;
-    private ExecutorService executor = Executors.newFixedThreadPool(128);
-    // players in queue
-    private Map<String, ClientConnection> playersConnections = new HashMap<>();
-    // lobbies
-    private Map<Game, ArrayList<ClientConnection>> lobbiesConnections = new HashMap<>();
-
-
 }
--- Code/src/main/java/it/polimi/ingsw/server/SocketClientConnection.java
+++ Code/src/main/java/it/polimi/ingsw/server/SocketClientConnection.java
@@ -1,4 +1,69 @@
 package it.polimi.ingsw.server;
 
-public class SocketClientConnection {
+import it.polimi.ingsw.observer.Observable;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketClientConnection extends Observable<String> implements ClientConnection, Runnable {
+
+    private Socket socket;
+    private ObjectOutputStream out;
+    private Server server;
+    private boolean active = true;
+
+    public SocketClientConnection(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    private synchronized boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public void closeConnection() {
+        send("Connection closed!");
+        try {
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("Error when closing socket!");
+        }
+        active = false;
+    }
+
+    private void close() {
+        closeConnection();
+        System.out.println("deregistering client...");
+        server.deregisterConnection(this);
+        System.out.println("Done!");
+    }
+
+    @Override
+    public void asyncSend(Object message) {
+        new Thread((Runnable) () -> {
+                send(message);
+            }).start();
+    }
+
+    private synchronized void send(Object message){
+        try {
+            out.reset();
+            out.writeObject(message);
+            out.flush();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        Scanner in;
+        String name;
+        try {
+            in = new Scanner(socket.getInputStream());
+        } catch ()
+    }
 }
