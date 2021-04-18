5ead34d4c549ce6dffca19bc6f1eca6fb7e439d7
P1224 <giacomo.pizza99@gmail.com>
2021-04-18 02:39:42 +0200

    Added local copy classes and methods for client side model. Classes: DevelopmentCard, LeaderCard

--- Code/src/main/java/it/polimi/ingsw/model/card/DevelopmentCard.java
+++ Code/src/main/java/it/polimi/ingsw/model/card/DevelopmentCard.java
@@ -3,12 +3,74 @@ package it.polimi.ingsw.model.card;
 import it.polimi.ingsw.model.card.color.Color;
 import it.polimi.ingsw.model.card.effect.ProductionEffect;
 import it.polimi.ingsw.model.card.requirement.Requirement;
+import it.polimi.ingsw.model.locally_copiable.LocallyCopyable;
 
-public class DevelopmentCard extends Card{
+public class DevelopmentCard extends Card implements LocallyCopyable<DevelopmentCard.DevelopmentCardLocalCopy> {
 
     private final int level;
     private final ProductionEffect effect;
     private final Color color;
+
+    public static class DevelopmentCardLocalCopy {
+
+        private final int level;
+        private final ProductionEffect effect;
+        private final Color color;
+        private final boolean active;
+        private final int victoryPoints;
+        private final Requirement requirement;
+
+        public DevelopmentCardLocalCopy(
+                int level,
+                ProductionEffect effect,
+                Color color,
+                boolean active,
+                int victoryPoints,
+                Requirement requirement) {
+            this.level = level;
+            this.effect = effect;
+            this.color = color;
+            this.active = active;
+            this.victoryPoints = victoryPoints;
+            this.requirement = requirement;
+        }
+
+        public int getLevel() {
+            return level;
+        }
+
+        public ProductionEffect getEffect() {
+            return effect;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public boolean isActive() {
+            return active;
+        }
+
+        public int getVictoryPoints() {
+            return victoryPoints;
+        }
+
+        public Requirement getRequirement() {
+            return requirement;
+        }
+    }
+
+    @Override
+    public DevelopmentCardLocalCopy getLocalCopy() {
+        return new DevelopmentCardLocalCopy(
+                this.level,
+                this.effect,
+                this.color,
+                this.isActive(),
+                this.getVictoryPoints(),
+                this.getRequirement()
+        );
+    }
 
     public DevelopmentCard(boolean active, int victoryPoints, int level, ProductionEffect effect, Color color, Requirement requirement) {
         super(active, victoryPoints, requirement);
