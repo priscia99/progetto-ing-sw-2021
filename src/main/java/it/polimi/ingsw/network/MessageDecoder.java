21067fc205219d75ec625355874099003b88b87f
Andrea Prisciantelli <priscia99@gmail.com>
2021-05-14 18:43:10 +0200

    Removed switch from MessageDecoder: FILL IT!

--- src/main/java/it/polimi/ingsw/network/MessageDecoder.java
+++ src/main/java/it/polimi/ingsw/network/MessageDecoder.java
@@ -16,29 +16,7 @@ public class MessageDecoder implements Observer<Message> {
 
     @Override
     public void update(Message object) {
-        ActionParams params;
-        switch(object.getType()){
-            case DROP_LEADER_CARD:
-                params = (DropLeaderCardParams) object.getPayload();
-                turnController.playerPickedAction(TurnActionType.DROP_LEADER_CARD, params);
-                break;
-            case PLAY_LEADER_CARD:
-                params = (PlayLeaderCardParams) object.getPayload();
-                turnController.playerPickedAction(TurnActionType.PLAY_LEADER_CARD, params);
-                break;
-            case BUY_DEVELOPMENT_CARD:
-                params = (BuyDevelopmentCardParams) object.getPayload();
-                turnController.playerPickedAction(TurnActionType.BUY_DEVELOPMENT_CARD, params);
-                break;
-            case PICK_RESOURCES:
-                params = (PickResourcesParams) object.getPayload();
-                turnController.playerPickedAction(TurnActionType.PICK_RESOURCES, params);
-                break;
-            case START_PRODUCTION:
-                params = (StartProductionParams) object.getPayload();
-                turnController.playerPickedAction(TurnActionType.START_PRODUCTION, params);
-                break;
-        }
+        // TODO: Fill me
     }
 
 }
