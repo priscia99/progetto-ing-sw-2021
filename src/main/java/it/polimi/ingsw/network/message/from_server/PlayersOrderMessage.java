61158cf6ae439d723d167fb9251886cb5c3870a1
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-05-30 11:50:36 +0200

    added player order message

---
+++ src/main/java/it/polimi/ingsw/network/message/from_server/PlayersOrderMessage.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.network.message.from_server;
+
+import it.polimi.ingsw.client.controller.ClientController;
+import it.polimi.ingsw.network.message.Message;
+import it.polimi.ingsw.server.model.game.Player;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class PlayersOrderMessage implements Message<ClientController>, Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private final ArrayList<String> playersOrder;
+
+    public PlayersOrderMessage(ArrayList<Player> players) {
+        this.playersOrder = new ArrayList<>();
+        //First player is in position 1 of array
+        for(int i = 1; i<players.size(); i++){
+            playersOrder.add(players.get(i).getNickname());
+        }
+        playersOrder.add(players.get(0).getNickname());
+    }
+
+    public void execute(ClientController target) {
+
+    }
+
+}
--- src/main/java/it/polimi/ingsw/server/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/server/model/game/Game.java
@@ -3,10 +3,7 @@ package it.polimi.ingsw.server.model.game;
 import it.polimi.ingsw.client.controller.ClientController;
 import it.polimi.ingsw.exceptions.UserNotFoundException;
 import it.polimi.ingsw.network.message.Message;
-import it.polimi.ingsw.network.message.from_server.CurrentPlayerMessage;
-import it.polimi.ingsw.network.message.from_server.ExceptionMessage;
-import it.polimi.ingsw.network.message.from_server.GameReadyMessage;
-import it.polimi.ingsw.network.message.from_server.LastRoundMessage;
+import it.polimi.ingsw.network.message.from_server.*;
 import it.polimi.ingsw.observer.Observable;
 import it.polimi.ingsw.observer.Observer;
 import it.polimi.ingsw.server.model.card.LeaderCard;
@@ -148,6 +145,7 @@ public class Game extends Observable<Message<ClientController>> implements Obser
         notify(new GameReadyMessage(ready));
         if(ready){
             setFirstPlayer();
+            notify(new PlayersOrderMessage(players));
             nextTurn();
         }
     }
