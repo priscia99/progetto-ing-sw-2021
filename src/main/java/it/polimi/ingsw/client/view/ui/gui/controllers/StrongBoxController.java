63b26e81d25afb9fa5e92fbb5ad4151ef4b3f68a
Andrea Prisciantelli <priscia99@gmail.com>
2021-06-26 19:18:50 +0200

    add buy development card action

--- src/main/java/it/polimi/ingsw/client/view/ui/gui/controllers/StrongBoxController.java
+++ src/main/java/it/polimi/ingsw/client/view/ui/gui/controllers/StrongBoxController.java
@@ -2,21 +2,32 @@ package it.polimi.ingsw.client.view.ui.gui.controllers;
 
 import it.polimi.ingsw.client.controller.ClientController;
 import it.polimi.ingsw.client.model.ClientStrongbox;
+import it.polimi.ingsw.client.view.ui.gui.scene.SceneController;
+import it.polimi.ingsw.server.model.resource.ResourceType;
+import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Glow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class StrongBoxController extends GenericGUIController {
 
     private GridPane strongboxPane;
     private TextField strongboxCoin, strongboxServant, strongboxShield, strongboxStone;
+    private Pane strongboxCoinIco, strongboxServantIco, strongboxShieldIco, strongboxStoneIco;
 
-    public StrongBoxController(ClientController clientController, GridPane strongboxPane, TextField strongboxCoin, TextField strongboxServant, TextField strongboxShield, TextField strongboxStone) {
+    public StrongBoxController(ClientController clientController, GridPane strongboxPane, TextField strongboxCoin, TextField strongboxServant, TextField strongboxShield, TextField strongboxStone, Pane strongboxCoinIco, Pane strongboxServantIco, Pane strongboxShieldIco, Pane strongboxStoneIco) {
         super(clientController);
         this.strongboxPane = strongboxPane;
         this.strongboxCoin = strongboxCoin;
         this.strongboxServant = strongboxServant;
         this.strongboxShield = strongboxShield;
         this.strongboxStone = strongboxStone;
+        this.strongboxCoinIco = strongboxCoinIco;
+        this.strongboxServantIco = strongboxServantIco;
+        this.strongboxShieldIco = strongboxShieldIco;
+        this.strongboxStoneIco = strongboxStoneIco;
     }
 
     /**
@@ -44,5 +55,83 @@ public class StrongBoxController extends GenericGUIController {
         strongboxShield.setText("0");
         strongboxStone.setText("0");
     }
+
+    public void setResourcesAsPickable(boolean isPickable){
+        if(isPickable) {
+            strongboxPane.setStyle("-fx-border-color: darkred;");
+            strongboxCoin.setText("1");
+            this.enableHandlers();
+        }else{
+            strongboxPane.setStyle("-fx-border: none;");
+            this.disableHandlers();
+        }
+
+    }
+
+    public void disableHandlers(){
+        strongboxCoinIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        strongboxCoinIco.setEffect(null);
+        strongboxServantIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        strongboxServantIco.setEffect(null);
+        strongboxShieldIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        strongboxShieldIco.setEffect(null);
+        strongboxStoneIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        strongboxStoneIco.setEffect(null);
+    }
+
+    public void enableHandlers(){
+        if(Integer.parseInt(strongboxCoin.getText())>0){
+            strongboxCoinIco.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+        else{
+            strongboxCoinIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+
+        if(Integer.parseInt(strongboxServant.getText())>0){
+            strongboxServantIco.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+        else{
+            strongboxServantIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+
+        if(Integer.parseInt(strongboxShield.getText())>0){
+            strongboxShieldIco.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+        else{
+            strongboxShieldIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+
+        if(Integer.parseInt(strongboxStone.getText())>0){
+            strongboxStoneIco.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+        else{
+            strongboxStoneIco.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClickedResource);
+        }
+    }
+
+    private final EventHandler<MouseEvent> onClickedResource = event -> {
+        Pane triggeredPane = (Pane)event.getSource();
+        ResourceType resourceType = null;
+        switch (triggeredPane.getId()){
+            case "coin-icon":       resourceType = ResourceType.COIN;
+                                    strongboxCoin.setText(String.valueOf(Integer.parseInt(strongboxCoin.getText()) - 1));
+                                    strongboxCoin.setEffect(new Glow(0.6));
+                                    break;
+            case "stone-icon":      resourceType = ResourceType.STONE;
+                                    strongboxStone.setText(String.valueOf(Integer.parseInt(strongboxStone.getText()) - 1));
+                                    strongboxStone.setEffect(new Glow(0.6));
+                                    break;
+            case "servant-icon":    resourceType = ResourceType.SERVANT;
+                                    strongboxServant.setText(String.valueOf(Integer.parseInt(strongboxServant.getText()) - 1));
+                                    strongboxServant.setEffect(new Glow(0.6));
+                                    break;
+            case "shield-icon":     resourceType = ResourceType.SHIELD;
+                                    strongboxShield.setText(String.valueOf(Integer.parseInt(strongboxShield.getText()) - 1));
+                                    strongboxShield.setEffect(new Glow(0.6));
+                                    break;
+        }
+        this.enableHandlers();
+        SceneController.getMainGUIController().getPickResourcesFromStorageController().addFromStrongbox(resourceType);
+    };
 
 }
